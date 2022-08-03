418d012923ede8137f1ccbf8ce773b573e3dcf7f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 01:44:40 -0700

    coded a Pair class to store the node and their level number

--- src/LevelOfNodes.java
+++ src/LevelOfNodes.java
@@ -1,5 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+class Pair{
+    int node;
+    int level;
+    Pair(int node, int level){
+        this.node = node;
+        this.level =level;
+    }
+}
+
+
 public class LevelOfNodes{
+
+    static int levelOfNode(ArrayList<ArrayList<Integer>> adj, int v, int x){
+        if(x==0){
+            return 0;
+        }
+
+
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
