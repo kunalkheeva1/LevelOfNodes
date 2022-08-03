107ba4905e013f646721d1b9287ae484c9091795
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 02:01:36 -0700

    coded a method which return a level of a particular node, please read the comments carefully to examine the depth of it

--- src/LevelOfNodes.java
+++ src/LevelOfNodes.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 
+//pair which stores the node and its level
 class Pair{
     int node;
     int level;
@@ -17,9 +18,14 @@ class Pair{
 public class LevelOfNodes{
 
     static int levelOfNode(ArrayList<ArrayList<Integer>> adj, int v, int x){
+
+        //if node is 0, which means the first element then return 0
         if(x==0){
             return 0;
         }
+
+        //get a queue and start storing the pairs
+        //to keep the record get a visited array too and initialize all of its values as not visited
         Queue<Pair> q =new LinkedList<>();
         Pair pair =new Pair(0,0);
         q.add(pair);
@@ -33,7 +39,11 @@ public class LevelOfNodes{
             Pair pp = q.remove();
             int current = pp.node;
             int level = pp.level;
+            //check for the particular connections of the node, and run a loop until the size of
+            //its connections
             for(int i =0; i<adj.get(current).size(); i++){
+                //check the connections , if not visited mark visited, then see if its equal to x
+                //if yes then return level +1 else get another pair
                 int connection = adj.get(current).get(i);
                 if(visited[connection]==0){
                     visited[connection]=1;
